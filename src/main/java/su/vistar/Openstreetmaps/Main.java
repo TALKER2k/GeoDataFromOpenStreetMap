131a1dd21b32f5a27aa71c7a9a8ebe2a74c0f62b
IlyaStenenko <ilyastenenko@gmail.com>
2024-03-18 18:23:56 +0300

    init project

---
+++ src/main/java/su/vistar/Openstreetmaps/Main.java
@@ -0,0 +1,12 @@
+package su.vistar.Openstreetmaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    //Test commit check
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
