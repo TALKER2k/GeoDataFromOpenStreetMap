07823df12458a281e14ea8074292074e9d620a6d
user <nunikasnikun26504228@yandex.ru>
2024-03-21 11:37:51 +0300

    настройка и миграция ликвибейз

--- backend/src/main/resources/db/changelog/1.0/170320241619_001_init_tables.sql
+++ backend/src/main/resources/db/changelog/1.0/170320241619_001_init_tables.sql
@@ -5,3 +5,28 @@ CREATE TABLE local_places_lift_gates
     lat      DOUBLE PRECISION,
     name     VARCHAR(100)
 );
+
+CREATE TABLE users
+(
+    id        BIGINT PRIMARY KEY,
+    username  VARCHAR(50),
+    city      VARCHAR(50),
+    region    VARCHAR(50),
+    country   VARCHAR(50),
+    latitude  DOUBLE PRECISION,
+    longitude DOUBLE PRECISION,
+    password  VARCHAR(100)
+);
+
+CREATE TABLE roles
+(
+    id        BIGINT PRIMARY KEY,
+    name VARCHAR(10)
+);
+
+
+CREATE TABLE user_roles
+(
+    user_id BIGINT REFERENCES users (id),
+    role_id BIGINT REFERENCES roles (id)
+);
