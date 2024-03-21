a4cbd004207ed574f84523eaeadecefa92cf6011
user <nunikasnikun26504228@yandex.ru>
2024-03-21 10:38:43 +0300

    спринг

--- backend/src/main/java/su/vistar/Openstreetmaps/models/Role.java
+++ backend/src/main/java/su/vistar/Openstreetmaps/models/Role.java
@@ -1,34 +1,4 @@
 package su.vistar.Openstreetmaps.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
-import lombok.Data;
-import org.springframework.security.core.GrantedAuthority;
-
-import java.util.Set;
-
-@Entity
-@Table(name = "roles")
-@Data
 public class Role {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinTable(
-            name = "user_roles",
-            joinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"),
-            inverseJoinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id")
-    )
-    @JsonIgnore
-    private Set<Employee> employees;
-
-    public Role(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Role() {}
 }
-
