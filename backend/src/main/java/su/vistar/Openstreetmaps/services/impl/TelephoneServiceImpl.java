cb1edbdabea904629087da32db6e3574d7ed0c7b
IlyaStenenko <ilyastenenko@gmail.com>
2024-03-25 18:11:50 +0300

    Refactor TelefonServiceImpl

--- backend/src/main/java/su/vistar/Openstreetmaps/services/impl/TelephoneServiceImpl.java
+++ backend/src/main/java/su/vistar/Openstreetmaps/services/impl/TelephoneServiceImpl.java
@@ -1,7 +1,9 @@
 package su.vistar.Openstreetmaps.services.impl;
 
+import org.springframework.stereotype.Service;
 import su.vistar.Openstreetmaps.services.TelephoneService;
 
+@Service
 public class TelephoneServiceImpl implements TelephoneService {
     @Override
     public void callByNumber(String telephone) {
