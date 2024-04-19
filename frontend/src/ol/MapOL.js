bbb296c0edb8eb43b34ce954d12333bfd257a7e5
user <nunikasnikun26504228@yandex.ru>
2024-04-19 22:19:56 +0300

    инициализация карты, маркеров, их чистка и белизна

--- frontend/src/ol/MapOL.js
+++ frontend/src/ol/MapOL.js
@@ -10,82 +10,112 @@ import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
 import { fromLonLat } from 'ol/proj';
 import { Icon, Style } from 'ol/style';
+import Overlay from 'ol/Overlay';
+import './MapStyle.css';
 
 class MapOL extends Component {
-    constructor(props) {
-        super(props);
-        this.mapRef = React.createRef();
-        this.map = null;
-        this.popup = null;
+  constructor(props) {
+    super(props);
+    this.mapRef = React.createRef();
+    this.map = null;
+    this.popup = null;
+  }
+
+  componentDidMount() {
+    const osmLayer = new TileLayer({
+      preload: Infinity,
+      source: new OSM(),
+    });
+
+    const vectorSource = new VectorSource();
+
+    const vectorLayer = new VectorLayer({
+      source: vectorSource,
+    });
+
+    this.map = new Map({
+      target: this.mapRef.current,
+      layers: [osmLayer, vectorLayer],
+      view: new View({
+        center: [0, 0],
+        zoom: 5,
+      }),
+    });
+
+    this.map.on('pointermove', this.handleMapPointerMove.bind(this));
+
+    this.popup = document.createElement('div');
+    this.popup.className = 'ol-popup';
+    this.mapRef.current.appendChild(this.popup);
+  }
+
+  componentWillUnmount() {
+    this.map.setTarget(null);
+  }
+
+  addMarker(lon, lat, number, name) {
+    const iconStyle = new Style({
+      image: new Icon({
+        src: 'https://openlayers.org/en/latest/examples/data/icon.png',
+      }),
+    });
+  
+    const marker = new Feature({
+      geometry: new Point(fromLonLat([lon, lat])),
+      number: number,
+      name: name,
+    });
+  
+    marker.setStyle(iconStyle);
+  
+    marker.on('pointerenter', (event) => {
+      const coordinates = event.coordinate;
+      const overlay = new Overlay({
+        position: coordinates,
+        positioning: 'center-center',
+        offset: [0, -25],
+      });
+      this.map.addOverlay(overlay);
+    });
+  
+    marker.on('pointerleave', () => {
+      this.map.getOverlays().clear();
+    });
+  
+    this.map.getLayers().item(1).getSource().addFeature(marker);
+    this.centerMap(lon, lat);
+  }
+  
+  centerMap(lon, lat) {
+    const view = this.map.getView();
+    view.setCenter(fromLonLat([lon, lat]));
+    view.setZoom(12);
+  }
+
+    clearMarkers() {
+    const vectorLayers = this.map.getLayers().getArray().filter(layer => layer instanceof VectorLayer);
+    vectorLayers.forEach(layer => {
+      layer.getSource().clear();
+    });
+  }
+
+  handleMapPointerMove(event) {
+    const pixel = this.map.getEventPixel(event.originalEvent);
+    const feature = this.map.forEachFeatureAtPixel(pixel, (feature) => feature);
+    if (feature) {
+      const coordinates = feature.getGeometry().getCoordinates();
+      this.popup.innerHTML = `<div>Phone: ${feature.get('number')}</div><div>Name: ${feature.get('name')}</div>`;
+      this.popup.style.display = 'block';
+      this.popup.style.left = `${event.pixel[0]}px`;
+      this.popup.style.top = `${event.pixel[1]}px`;
+    } else {
+      this.popup.style.display = 'none';
     }
+  }
 
-    componentDidMount() {
-        const osmLayer = new TileLayer({
-            preload: Infinity,
-            source: new OSM(),
-        });
-
-        const vectorSource = new VectorSource();
-
-        const vectorLayer = new VectorLayer({
-            source: vectorSource,
-        });
-
-        this.map = new Map({
-            target: this.mapRef.current,
-            layers: [osmLayer, vectorLayer],
-            view: new View({
-                center: [0, 0],
-                zoom: 5,
-            }),
-        });
-
-        this.map.on('pointermove', this.handleMapPointerMove.bind(this));
-
-        this.popup = document.createElement('div');
-        this.popup.className = 'ol-popup';
-        this.mapRef.current.appendChild(this.popup);
-    }
-
-    componentWillUnmount() {
-        this.map.setTarget(null);
-    }
-
-    addMarker(lon, lat, number, name) {
-        const iconStyle = new Style({
-            image: new Icon({
-                src: 'https://openlayers.org/en/latest/examples/data/icon.png',
-            }),
-        });
-
-        const marker = new Feature({
-            geometry: new Point(fromLonLat([lon, lat])),
-            number: number,
-            name: name,
-        });
-
-        marker.setStyle(iconStyle);
-
-        this.map.getLayers().item(1).getSource().addFeature(marker);
-    }
-
-    handleMapPointerMove(event) {
-        const pixel = this.map.getEventPixel(event.originalEvent);
-        const feature = this.map.forEachFeatureAtPixel(pixel, (feature) => feature);
-        if (feature) {
-            const coordinates = feature.getGeometry().getCoordinates();
-            this.popup.innerHTML = `<div>${feature.get('number')}</div><div>${feature.get('name')}</div>`;
-            this.popup.style.display = 'block';
-            this.popup.style.left = `${event.pixel[0]}px`;
-            this.popup.style.top = `${event.pixel[1]}px`;
-        } else {
-            this.popup.style.display = 'none';
-        }
-    }
-
-    render() {
-        return <div ref={this.mapRef} style={{ height: '80vh', width: '100%' }} />;
-    }
+  render() {
+    return <div ref={this.mapRef} style={{ height: '90vh', width: '100%' }} />;
+  }
 }
 
 export default MapOL;
