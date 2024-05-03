0de6d82d4eb24500ab253971d56873a849ee888a
IlyaStenenko <ilyastenenko@gmail.com>
2024-05-03 16:02:55 +0300

    Route

---
+++ frontend/src/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+import Switch, {Component} from 'react';
+import Browser from "leaflet/src/core/Browser";
+
+import GatePage from './page/GatePage'
+import RegistrationPage from './page/RegistrationPage'
+import RoutePage from './page/RoutePage'
+import SignUpPage from './page/SignUpPage'
+
+
+export default class Header extends Component{
+    render() {
+        return(
+            <div>
+                <Navbar expand="lg" className="bg-body-tertiary">
+                    <Container>
+                        <Navbar.Brand href="#home">OSMVIEW</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link href="/GatePage">Gate service</Nav.Link>
+                                <Nav.Link href="/RoutePage">Route service</Nav.Link>
+                                <Nav.Link href="/SignUpPage">Sign up</Nav.Link>
+                                <Nav.Link href="/RegistrationPage">Registration</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+                <Router>
+                    <Routes>
+                        <Route path="/GatePage" element={<GatePage/>} />
+                        <Route path="/RegistrationPage" element={<RegistrationPage/>} />
+                        <Route path="/RoutePage" element={<RoutePage/>} />
+                        <Route path="/SignUpPage" element={<SignUpPage/>} />
+                    </Routes>
+                </Router>
+            </div>
+        );
+    }
+}
---
+++ frontend/src/page/RegistrationPage.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class RegistrationPage extends Component{
+    render() {
+        return(
+            <div>
+                RegistrationPage
+            </div>
+        )
+    }
+}
